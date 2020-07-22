d369ae22b90248907a82969a4e8e93be1d1fbce1
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-07-21 22:38:15 -0300

    Delete method

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transactionId = await transactionsRepository.findOne(id);
+
+    if (!transactionId) {
+      throw new AppError('Transaction id does not exist', 400);
+    }
+
+    await transactionsRepository.remove(transactionId);
   }
 }
 
